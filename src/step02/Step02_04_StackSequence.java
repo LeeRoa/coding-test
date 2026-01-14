57463fd9bd0135a065744bd315ecfdb3a75355a1
roa7717 <roa7717@kcert.co.kr>
2026-01-14 10:14:33 +0900

    stack 직접 구현한 괄호 짝 찾기

--- src/step02/Step02_04_StackSequence.java
+++ src/step02/Step02_04_StackSequence.java
@@ -3,7 +3,6 @@ package step02;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Step02_04_StackSequence {
