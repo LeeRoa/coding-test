43bd8aaf67aa99ac2d65aa79c9ccfb7d769e4daf
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-01 22:47:32 +0900

    백준 1966문제 해결

--- src/step03/BaekJoon_1966.java
+++ src/step03/BaekJoon_1966.java
@@ -11,41 +11,55 @@ public class BaekJoon_1966 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 테스트 케이스의 개수
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 문서의 총 개수
-        int N = Integer.parseInt(st.nextToken());
-        // 몇 번째로 인쇄되었는지 궁금한 타겟 문서
-        int M = Integer.parseInt(st.nextToken());
 
-        int result = 0;
         for (int testcase = 0; testcase < T; testcase++) {
-            // 프린터 큐 생성
-            Queue<Integer> queue = new ArrayDeque<>();
+            int count = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            // 문서들의 현 상태 (중요도)
-            int[] docArr = new int[N];
+            // 문서의 총 개수
+            int N = Integer.parseInt(st.nextToken());
+            // 몇 번째로 인쇄되었는지 궁금한 타겟 문서
+            int M = Integer.parseInt(st.nextToken());
+
+            // 프린터 큐 생성
+            Queue<Integer> printQueue = new ArrayDeque<>();
+            Queue<Integer> docQueue = new ArrayDeque<>();
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
 
             // 큐에 문서 삽입
             for (int i = 0; i < N; i++) {
                 // [1, 1, 9, 1, 1, 1]
-                docArr[i] = Integer.parseInt(st.nextToken());
+                docQueue.offer(i);
+                printQueue.offer(Integer.parseInt(st2.nextToken()));
             }
 
             // 프린터 작동 (모든 문서가 출력 될때 까지)
-            while (queue.size() != 0) {
-                // 현재 문서를 담을 곳
-                int now;
+            while (!printQueue.isEmpty()) {
+                // 프린터 큐 순회
+                for (int print : printQueue) {
+                    boolean isPrint = true;
 
-                for (int i = 1; i < queue.size(); i++) {
-                    // 첫번째 문서 docArr[0]
-                    now = docArr[i - 1];
+                    // 다른 문서들과 비교
+                    for (int after : printQueue) {
+                        if (print < after) {
+                            isPrint = false;
+                            break;
+                        }
+                    }
 
-                    // 첫번째 문서가 그 뒤에 올 문서들보다 중요도가 낮다면 뒤로 밀기
-                    if (now < docArr[i]) {
-                        // 내 타겟을 앞으로 땡김 (원형 큐이기때문에 나머지 연산)
-                        M = (M - 1) % N;
-                    } else if (now >= queue.peek()) {
+                    // 현재 프린터 출력하기
+                    if (isPrint) {
+                        count++;
+                        int value = docQueue.poll();
+                        printQueue.poll();
 
+                        if (value == M) {
+                            System.out.println(count);
+                        }
+                    } else {
+                        // 뒤로 밀기
+                        docQueue.offer(docQueue.poll());
+                        printQueue.offer(printQueue.poll());
                     }
                 }
             }
