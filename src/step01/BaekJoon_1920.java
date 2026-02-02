6a5fe827f52e666a40a26abbd19b2d4445b0c4bf
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-02 18:47:18 +0900

    백준 1920번 진행

---
+++ src/step01/BaekJoon_1920.java
@@ -0,0 +1,11 @@
+package step01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon_1920 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
