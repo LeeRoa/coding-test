1a3ad97bb8683301fb820a77766b3b47e2484404
roa7717 <roa7717@kcert.co.kr>
2026-02-07 23:44:57 +0900

    백준 9375번 문재 진행

---
+++ src/BaekJoon_9375.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class BaekJoon_9375 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcase = Integer.parseInt(br.readLine());
+        // 테스트 케이스
+        for (int test = 0; test < testcase; test++) {
+            // 가진 의상 수
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 0; i < n; i++) {
+
+            }
+        }
+    }
+}
