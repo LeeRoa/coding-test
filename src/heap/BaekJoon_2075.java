c73053217d48f4933793a6dc0ee5bfbb9da4e054
roa7717 <roa7717@kcert.co.kr>
2026-02-10 18:44:54 +0900

    백준 2075번 문제 해결

--- src/heap/BaekJoon_2075.java
+++ src/heap/BaekJoon_2075.java
@@ -2,6 +2,7 @@ package heap;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,8 +10,8 @@ public class BaekJoon_2075 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // 우선순위를 큰 수부터
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         // 5번
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,7 +22,9 @@ public class BaekJoon_2075 {
             }
         }
 
-
-
+        for (int i = 0; i < N - 1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.poll());
     }
 }
