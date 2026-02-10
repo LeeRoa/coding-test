84f6b2431bef842fd2411aa6d088a075b1dd4ec1
roa7717 <roa7717@kcert.co.kr>
2026-02-10 10:47:05 +0900

    백준 11286번 문제 진행

---
+++ src/heap/BaekJoon_11286.java
@@ -0,0 +1,19 @@
+package heap;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon_11286 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 연산의 개수
+        int N = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < N; i++) {
+
+        }
+    }
+}
