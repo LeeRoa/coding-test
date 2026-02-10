e43c5b1135a5654a921a773c6454f05465c8dbf1
roa7717 <roa7717@kcert.co.kr>
2026-02-10 12:34:08 +0900

    백준 11286번 절댓값 힙 해결

--- src/heap/BaekJoon_11286.java
+++ src/heap/BaekJoon_11286.java
@@ -2,6 +2,7 @@ package heap;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class BaekJoon_11286 {
 
@@ -12,8 +13,35 @@ public class BaekJoon_11286 {
         int N = Integer.parseInt(br.readLine());
 
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>((s1, s2) -> {
+            if (Math.abs(s1) == Math.abs(s2)) {
+                if (s1 > s2) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                 if (Math.abs(s1) < Math.abs(s2)) {
+                     // 더 작은것을 내보낸다
+                     return -1;
+                 }
+                return 1;
+            }
+        });
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
 
+            if (x != 0) {
+                pq.offer(x);
+            } else {
+                if (pq.isEmpty()) {
+                    sb.append(x).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
         }
+        System.out.println(sb);
     }
 }
