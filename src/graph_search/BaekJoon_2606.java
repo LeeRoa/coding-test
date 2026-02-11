0b5d70dd2af02794a0746ca2602f618e3adb69ed
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-11 22:46:02 +0900

    백준 2606번 해결

---
+++ src/graph_search/BaekJoon_2606.java
@@ -0,0 +1,45 @@
+package graph_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BaekJoon_2606 {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int count = -1;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수 (노드)
+        int M = Integer.parseInt(br.readLine()); // 컴퓨터 쌍의 수 (간선)
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        visited = new boolean[N + 1];
+        dfs(1);
+
+        System.out.println(count);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        count++;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
