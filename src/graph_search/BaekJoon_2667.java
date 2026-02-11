8b3821f849ed9b2b7b2cb1a6c6462af7b3ad89e5
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-11 23:44:58 +0900

    백준 2667번 문제 진행

---
+++ src/graph_search/BaekJoon_2667.java
@@ -0,0 +1,14 @@
+package graph_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_2667 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+    }
+}
