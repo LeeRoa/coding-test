08b88729bc204d9ec046cd031f81fc4b5f8db18a
roa7717 <roa7717@kcert.co.kr>
2026-02-12 13:50:32 +0900

    백준 2667번 문제 해결

--- src/graph_search/BaekJoon_2667.java
+++ src/graph_search/BaekJoon_2667.java
@@ -3,36 +3,27 @@ package graph_search;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class BaekJoon_2667 {
-    static class Node {
-        int x;
-        int y;
-        ArrayList<Integer> adjacent;
-        boolean visited;
 
-        Node (int x, int y) {
-            this.x = x;
-            this.y = y;
-            adjacent = new ArrayList<>();
-            visited = false;
-        }
-    }
+    static boolean[][] maps; // 그래프 역할
+    static boolean[][] visited; // 방문기록
+    static int count = 0;
+    static int N;
 
-    static ArrayList<Node> graph; // 동적 사이즈 그래프 생성
-    static boolean[][] maps;
-
+    // 꼭 외우자 상하 좌우를 이동하기 위한 요소 두개
+    static int[] dx = {-1, 1, 0, 0}; // 행
+    static int[] dy = {0, 0, -1, 1}; // 열
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-
-        // 그래프 생성
-        graph = new ArrayList<>();
+        N = Integer.parseInt(br.readLine());
 
         // 지도 생성
         maps = new boolean[N][N];
+        visited = new boolean[N][N];
 
         // 단지 탐색
         for (int x = 0; x < N; x++) {
@@ -40,40 +31,54 @@ public class BaekJoon_2667 {
 
             // 지도 한줄씩 탐색 후 값 세팅
             for (int y = 0; y < N; y++) {
-                maps[x][y] = row[y] == '1' ? true : false; // row의 값을 하나씩 지도에 세팅한다 0, 1 판별
-                graph.add(new Node(x, y)); // 집이 있는 만큼 노드 생성
+                maps[x][y] = row[y] == '1'; // row의 값을 하나씩 지도에 세팅한다 0, 1 판별
+            }
+        }
+        List<Integer> sizes = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // 지도에 집이 있다면 dfs
+                if (maps[i][j] && !visited[i][j] ) {
+                    count = 0;
+                    dfs(i, j);
+                    sizes.add(count);
+                }
             }
         }
 
-        // 인접 노드 있는지 탐색 후 간선 생성
-        for (int i = 0; i < graph.size(); i++) {
-            Node node = graph.get(i);
-            int x = node.x;
-            int y  = node.y;
+        Collections.sort(sizes);
+        sb.append(sizes.size()).append("\n");
 
-            if (x - 1 >= 0) {
-                // 인접한 노드가 있다면 간선 생성
-                if (maps[x - 1][y]) {
+        for (int size : sizes) {
+            sb.append(size).append("\n");
+        }
+        System.out.println(sb);
+    }
 
+    // 탐색
+    static void dfs(int x, int y) {
+        count++;
+//        System.out.println("현재 위치: " + x + ", " + y);
+        // 해당 집 방문 체크
+        visited[x][y] = true;
+
+        // 상하좌우 이동 체크
+        for (int i = 0; i < 4; i++) {
+//            System.out.println("새로운 탐색 정보:x + dx[i] :  " + x + " + " + dx[i]);
+//            System.out.println("새로운 탐색 정보:y + dy[i] :  " + y + " + " + dy[i]);
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if (newX >= 0 && newX < N) {
+                if (maps[newX][y] && !visited[newX][y]) {
+//                    System.out.println("다음 갈 위치: " + newX + ", " + y);
+                    dfs(newX, y);
                 }
             }
 
-            if (y - 1 >= 0) {
-                // 인접한 노드가 있다면 간선 생성
-                if (maps[x][y - 1]) {
-
-                }
-            }
-
-            if (x + 1 <= N) {
-                if (maps[x + 1][y]) {
-
-                }
-            }
-
-            if (y + 1 <= N) {
-                if (maps[x][y + 1]) {
-
+            if (newY >= 0 && newY < N) {
+                if (maps[x][newY] && !visited[x][newY]) {
+//                    System.out.println("다음 갈 위치: " + x + ", " + newY);
+                    dfs(x, newY);
                 }
             }
         }
