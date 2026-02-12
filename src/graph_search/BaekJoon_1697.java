72714b47db41da4bc0613d1c7a475024aff055ca
roa7717 <roa7717@kcert.co.kr>
2026-02-12 18:24:03 +0900

    백준 1697번 진행

---
+++ src/graph_search/BaekJoon_1697.java
@@ -0,0 +1,11 @@
+package graph_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon_1697 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
