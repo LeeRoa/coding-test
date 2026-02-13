58c90ce1beb3d40bc367d73042f1362f80f33ea2
roa7717 <roa7717@kcert.co.kr>
2026-02-13 11:25:29 +0900

    백준 7562 진행

--- src/graph_search/BaekJoon_7562.java
+++ src/graph_search/BaekJoon_7562.java
@@ -10,6 +10,8 @@ public class BaekJoon_7562 {
     static int endCol, endRow;
     static int[][] maps;
     static boolean[][] visited;
+    static int[] dCol = {-2, 2, -2, 2, -1, 1, -1, 1};
+    static int[] dRow = {-1, 1, 1, -1, -2, 2, 2, -2};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,7 +38,35 @@ public class BaekJoon_7562 {
         }
     }
 
+    // 나이트의 이동
     static void bfs(int col, int row) {
+        Queue<Integer[]> queue = new ArrayDeque<>();
+        visited[col][row] = true;
+        queue.offer(new Integer[]{col, row});
 
+        while (!queue.isEmpty()) {
+            Integer[] now = queue.poll();
+            nowCol = now[0];
+            nowRow = now[1];
+
+            if (nowCol == endCol && nowRow == endCol) {
+                System.out.println(maps[nowCol][endCol]);
+                break;
+            }
+
+            // 나이트 이동
+            int newCol, newRow;
+            for (int i = 0; i < dCol.length; i++) {
+                newCol = nowCol + dCol[i];
+                newRow = nowRow + dRow[i];
+
+                if (newCol >= 0 && newCol < I
+                        && newRow >= 0 && newRow < I && !visited[newCol][newRow]) {
+                    maps[newCol][newRow] = maps[nowCol][nowRow] + 1;
+                    visited[newCol][newRow] = true;
+                    queue.offer(new Integer[]{newCol, newRow});
+                }
+            }
+        }
     }
 }
