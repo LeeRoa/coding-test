4a9a2840c8e77c7a53fefb2429bb70eebedda4cb
roa7717 <roa7717@kcert.co.kr>
2026-02-13 14:37:51 +0900

    백준 7576번 문제 해결 꼼꼼함 필요

--- src/graph_search/BaekJoon_7576.java
+++ src/graph_search/BaekJoon_7576.java
@@ -2,9 +2,7 @@ package graph_search;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BaekJoon_7576 {
     static int COL, ROW;
@@ -12,6 +10,19 @@ public class BaekJoon_7576 {
     static boolean[][] visited;
     static int[] dCol = {-1, 1, 0, 0};
     static int[] dRow = {0, 0, -1, 1};
+
+    static int startRow, startCol;
+    static ArrayList<Tomato> start = new ArrayList<>();
+
+    static class Tomato {
+        int row;
+        int col;
+
+        Tomato(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,36 +40,81 @@ public class BaekJoon_7576 {
         for (int row = 0; row < ROW; row++) {
             st = new StringTokenizer(br.readLine());
             for (int col = 0; col < COL; col++) {
-                box[row][col] = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 1) {
+                    Tomato tomato = new Tomato(row, col);
+                    start.add(tomato);
+                }
+                box[row][col] = num;
             }
         }
 
-        bfs(0, 0);
+        if (start.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            Tomato tomato = start.get(0);
+            bfs(tomato.row, tomato.col);
+
+            boolean ok = true;
+            for (int row = 0; row < ROW; row++) {
+                for (int col = 0; col < COL; col++) {
+                    if (box[row][col] == 0) {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+
+            int max = 0;
+            for (int i = 0; i < box.length; i++) {
+                for (int j = 0; j < box[i].length; j++) {
+                    max = Math.max(box[i][j], max);
+                }
+            }
+
+            if (!ok) {
+                System.out.println(-1);
+            } else {
+                System.out.println(max - 1);
+            }
+        }
     }
 
     static void bfs(int row, int col) {
-        Queue<Integer[]> queue = new ArrayDeque<>();
-        visited[row][col] = true;
-        queue.offer(new Integer[]{row, col});
+        Queue<Tomato> queue = new ArrayDeque<>();
+        for (Tomato tomato : start) {
+            visited[row][col] = true;
+            queue.offer(tomato);
+        }
 
         while (!queue.isEmpty()) {
-            Integer[] here = queue.poll();
-            int hereRow = here[0];
-            int hereCol = here[1];
-            // 토마토 상태 확인
-            int status = box[hereRow][hereCol];
+//            check();
+            Tomato here = queue.poll();
+            int hereRow = here.row;
+            int hereCol = here.col;
 
-            if (status == 1) {
-                // 익음
-                for (int i = 0; i < 4; i++) {
-                    int newRow = hereRow + dRow[i];
-                    int newCol = hereCol + dCol[i];
+            // 주변 토마토 탐색
+            for (int i = 0; i < 4; i++) {
+                int newRow = hereRow + dRow[i];
+                int newCol = hereCol + dCol[i];
+
+                if (newRow >= 0 && newRow < ROW
+                        && newCol >= 0 && newCol < COL
+                        && box[newRow][newCol] == 0
+                        && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = true;
+                    box[newRow][newCol] = box[hereRow][hereCol] + 1;
+                    queue.offer(new Tomato(newRow, newCol));
                 }
-            } else if (status == 0) {
-                // 안익음
-            } else {
-                // 없음
             }
         }
+    }
+
+    static void check() {
+        System.out.println("=========================================");
+        for (int i = 0; i < ROW; i++) {
+            System.out.println(Arrays.toString(box[i]));
+        }
+        System.out.println("=========================================");
     }
 }
