227b5d8adbb30896a6f4bedd0295e47d878bf9fd
roa7717 <roa7717@kcert.co.kr>
2026-02-16 14:22:12 +0900

    2206번 해결 - 그대신복습 너무너무 필요함

--- src/graph_search/BaekJoon_2206.java
+++ src/graph_search/BaekJoon_2206.java
@@ -8,9 +8,22 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BaekJoon_2206 {
+    static class Status {
+        int row;
+        int col;
+        int count;
+        int broken;
+
+        Status (int row, int col, int count, int broken) {
+            this.row = row;
+            this.col = col;
+            this.count = count;
+            this.broken = broken;
+        }
+    }
     static int ROW, COL;
     static int[][] map;
-    static boolean[][] visited;
+    static boolean[][][] visited;
     static int[] dCol = {-1, 1, 0, 0};
     static int[] dRow = {0, 0, -1, 1};
     static ArrayList<Integer[]> walls = new ArrayList<>();
@@ -21,6 +34,7 @@ public class BaekJoon_2206 {
         COL = Integer.parseInt(st.nextToken());
 
         map = new int[ROW][COL];
+        visited = new boolean[ROW][COL][2];
 
         for (int row = 0; row < ROW; row++) {
             char[] cols = br.readLine().toCharArray();
@@ -34,43 +48,47 @@ public class BaekJoon_2206 {
             }
         }
 
-        // 벽을 하나씩 없애는 순회
-        int[] counts = new int[walls.size()];
-        for (int i = 0; i < walls.size(); i++) {
-            visited = new boolean[ROW][COL];
-            bfs(0, 0);
-        }
+        System.out.println(bfs(0, 0));
     }
 
     // 1은 벽
     // 0이 통로
     // 한 번 부수는것이 가능
-    static void bfs(int row, int col) {
-        Queue<Integer[]> queue = new ArrayDeque<>();
-        queue.offer(new Integer[]{row, col});
-        visited[row][col] = true;
+    static int bfs(int row, int col) {
+        Queue<Status> queue = new ArrayDeque<>();
+        queue.offer(new Status(row, col, 1, 0));
+        visited[row][col][0] = true;
 
         while (!queue.isEmpty()) {
-            Integer[] hereRowCol = queue.poll();
-            int hereRow = hereRowCol[0];
-            int hereCol = hereRowCol[1];
-            int here = map[hereRow][hereCol];
+            Status here = queue.poll();
+            int hereRow = here.row;
+            int hereCol = here.col;
 
-            if (hereRow == ROW && hereCol == COL) {
-                break;
+            if (hereRow == ROW - 1 && hereCol == COL - 1) {
+                return here.count;
             }
 
             for (int i = 0; i < 4; i++) {
-                int targetRow = hereRow + dRow[i];
-                int targetCol = hereCol + dCol[i];
+                int nRow = hereRow + dRow[i];
+                int nCol = hereCol + dCol[i];
 
-                if (targetRow >= 0 && targetRow < ROW && targetCol >= 0 && targetCol < COL
-                        && map[targetRow][targetCol] == 0) {
-                    queue.offer(new Integer[] {targetRow, targetCol});
-                    visited[targetRow][targetCol] = true;
-                    map[targetRow][targetCol] = here + 1;
+                if (nRow >= 0 && nRow < ROW && nCol >= 0 && nCol < COL) {
+
+                    // 벽인 경우
+                    if (map[nRow][nCol] == 1) {
+                        if (here.broken == 0 && !visited[nRow][nCol][1]) {
+                            visited[nRow][nCol][1] = true;
+                            queue.offer(new Status(nRow, nCol, here.count + 1, 1));
+                        }
+                    } else if (map[nRow][nCol] == 0 && !visited[nRow][nCol][here.broken]) {
+                        // 길인 경우
+                        visited[nRow][nCol][here.broken] = true;
+                        queue.offer(new Status(nRow, nCol, here.count + 1, here.broken));
+                    }
                 }
             }
         }
+
+        return -1;
     }
 }
