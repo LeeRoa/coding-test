8430b54a5d056be3fd63bfd598108e62405ee9a8
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-18 01:55:49 +0900

    백준 진행 중

--- src/graph_search/backtracking/BaekJoon_14888.java
+++ src/graph_search/backtracking/BaekJoon_14888.java
@@ -1,8 +1,21 @@
 package graph_search.backtracking;
 
+import java.io.*;
+import java.util.*;
+
 public class BaekJoon_14888 {
-
+    static int N;
+    static boolean[] V;
+    static int[] nums;
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+        V = new boolean[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
     }
 }
