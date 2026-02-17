ae73e6803afa846244ce4a693144cef7b4b93faf
roa7717 <roa7717@kcert.co.kr>
2026-02-17 13:22:27 +0900

    백준 2468번 해결

--- src/graph_search/dfs/BaekJoon_2468.java
+++ src/graph_search/dfs/BaekJoon_2468.java
@@ -2,7 +2,7 @@ package graph_search.dfs;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BaekJoon_2468 {
     static int N;
@@ -26,16 +26,58 @@ public class BaekJoon_2468 {
             }
         }
 
-        visited = new boolean[N][N];
-        dfs(0, 0);
+        List<Integer> counts = new ArrayList<>();
+        // 점점높이를 증가
+        for (int i = 0; i < 100; i++) {
+            visited = new boolean[N][N];
+            for (int row = 0; row < N; row++) {
+                for (int col = 0; col < N; col++) {
+                    map[row][col] = map[row][col] - 1 < 0 ? 0 : map[row][col] - i;
+                }
+            }
+
+//            System.out.println("비왔다. 강수량 : " + i);
+//            for (int[] nums : map) {
+//                System.out.println(Arrays.toString(nums));
+//            }
+//            System.out.println("====================================");
+            int count = 0;
+
+            for (int row = 0; row < N; row++) {
+                for (int col = 0; col < N; col++) {
+                    if (!visited[row][col] && map[row][col] > 0) {
+                        dfs(row, col);
+                        count++;
+                    }
+                }
+
+            }
+//            System.out.println("영역개수 : "  + count);
+//            System.out.println("====================================");
+            counts.add(count);
+
+            for (int row = 0; row < N; row++) {
+                for (int col = 0; col < N; col++) {
+                    map[row][col] = map[row][col] - 1 < 0 ? 0 : map[row][col] + i;
+                }
+            }
+        }
+
+//        System.out.println(counts);
+        Collections.sort(counts);
+
+        System.out.println(counts.get(counts.size() - 1));
     }
 
     static void dfs(int row, int col) {
+//        System.out.println("탐색 영역 : " + row + ", " + col);
         visited[row][col] = true;
 
         for (int i = 0; i < 4; i++) {
             int nextRow = row + dRow[i];
             int nextCol = col + dCol[i];
+
+            if (nextRow >= N || nextRow < 0 || nextCol >= N || nextCol < 0) continue;
 
             if (!visited[nextRow][nextCol] && map[nextRow][nextCol] > 0) {
                 dfs(nextRow, nextCol);
