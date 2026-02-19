cb8a124bf6738c71ba0b03ccfbd44a9298eef09d
roa7717 <roa7717@kcert.co.kr>
2026-02-19 09:50:20 +0900

    구간합 기초복습

--- src/prefix_sum/BaekJoon_11659_1.java
+++ src/prefix_sum/BaekJoon_11659_1.java
@@ -1,42 +1,32 @@
-package sum;
+package prefix_sum;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class BaekJoon_11659 {
+public class BaekJoon_11659_1 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        // [5, 4, 3, 2, 1]
-        int[] nums = new int[N + 1];
-        // [5, 9, 12, 14, 15]
-        int[] sums = new int[N + 1];
+        int[] A = new int[N + 1];
+        int[] S = new int[N + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N + 1; i++) {
-            if (i == 0) continue;
+        for (int i = 1; i <= N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            nums[i] = num;
-            if (i == 1) {
-                sums[i] = num;
-            } else {
-                sums[i] = num + sums[i - 1];
-            }
+            A[i] = num;
+            S[i] = S[i - 1] + A[i];
         }
 
-        System.out.println(Arrays.toString(nums));
-        System.out.println(Arrays.toString(sums));
+//        System.out.println(Arrays.toString(A));
+//        System.out.println(Arrays.toString(S));
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            sb.append(sums[end] - sums[start - 1]).append("\n");
+            sb.append(S[end] - S[start - 1]).append("\n");
         }
 
         System.out.println(sb);
