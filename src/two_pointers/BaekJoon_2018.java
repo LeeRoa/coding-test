8f747c542b1437fce9cb369009b67de8bfe02584
roa7717 <roa7717@kcert.co.kr>
2026-02-20 09:09:57 +0900

    임시 커밋

--- src/two_pointers/BaekJoon_2018.java
+++ src/two_pointers/BaekJoon_2018.java
@@ -8,27 +8,24 @@ public class BaekJoon_2018 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N + 1];
-        int[] S = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
             nums[i] = i + 1;
-            S[i] = S[i - 1] + nums[i];
         }
         System.out.println(Arrays.toString(nums));
 
         int start = 0;
         int end = 0;
         int count = 0;
-        while (start != nums.length - 1) {
-            System.out.println("start: " + nums[start] + ", end: " + nums[end] + " = " + (nums[start] + nums[end] == 15));
-            int sum = nums[start] + nums[end];
+        int sum = 0;
+        while (end != N - 1) {
             if (sum == N) {
                 count++;
-                start = start + 1 > nums.length - 1 ? start : start + 1;
+                end++;
             } else if (sum > N) {
-                start = start + 1 > nums.length - 1 ? start : start + 1;
+                start++;
             } else {
-                end = end + 1 > nums.length - 1 ? end : end + 1;
+                end++;
             }
         }
         System.out.println(count);
