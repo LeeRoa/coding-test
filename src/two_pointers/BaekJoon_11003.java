53b5c01924c21387ee7fe20bb36cf100f4e95092
roa7717 <roa7717@kcert.co.kr>
2026-02-20 16:09:31 +0900

    11003번 문제 진행

---
+++ src/two_pointers/BaekJoon_11003.java
@@ -0,0 +1,10 @@
+package two_pointers;
+
+import java.io.*;
+import java.util.*;
+
+public class BaekJoon_11003 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
