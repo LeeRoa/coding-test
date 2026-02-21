4eee3d135dc325659a546cebd823956afe671d23
roa7717 <roa7717@kcert.co.kr>
2026-02-21 18:33:42 +0900

    백준 11003번 진행

--- src/two_pointers/BaekJoon_11003.java
+++ src/two_pointers/BaekJoon_11003.java
@@ -4,7 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class BaekJoon_11003 {
+
+    static class Node {
+        int index;
+        int data;
+
+        Node(int index, int data) {
+            this.index = index;
+            this.data = data;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        Deque<Node> queue = new ArrayDeque<>();
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+
+            if (queue.isEmpty()) queue.offer(new Node(i, input));
+
+            while (!queue.isEmpty()) {
+                Node root = queue.peek(); // 1
+
+                if (i - root.index > L) { // 1 - 0 > 3 ?
+                    queue.poll(); // 1 제거
+                } else { // 범위 안이라면
+                    if (root.data > input) { // 1 > 5 ?
+                        queue.poll(); // 들어온값보다 크면 기존 데이터 제거
+                    } else if (root.data < input) { // 1 < 5 ?
+                        sb.append(root.data).append(" "); // 기존 데이터 출력
+                        break;
+                    }
+                    queue.offer(new Node(i, input));
+                }
+            }
+        }
+
+        System.out.println(sb);
     }
 }
