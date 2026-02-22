9f7bf6f7b079c0fdd366c2b568f363ce5a8b977c
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-22 22:37:52 +0900

    11003번 해결

--- src/two_pointers/BaekJoon_11003.java
+++ src/two_pointers/BaekJoon_11003.java
@@ -13,6 +13,11 @@ public class BaekJoon_11003 {
             this.index = index;
             this.data = data;
         }
+
+        @Override
+        public String toString() {
+            return data + " ";
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -20,28 +25,44 @@ public class BaekJoon_11003 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-        Deque<Node> queue = new ArrayDeque<>();
+        Deque<Node> deque = new ArrayDeque<>();
 
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             int input = Integer.parseInt(st.nextToken());
+            Node node = new Node(i, input);
 
-            if (queue.isEmpty()) queue.offer(new Node(i, input));
-
-            while (!queue.isEmpty()) {
-                Node root = queue.peek(); // 1
-
-                if (i - root.index > L) { // 1 - 0 > 3 ?
-                    queue.poll(); // 1 제거
-                } else { // 범위 안이라면
-                    if (root.data > input) { // 1 > 5 ?
-                        queue.poll(); // 들어온값보다 크면 기존 데이터 제거
-                    } else if (root.data < input) { // 1 < 5 ?
-                        sb.append(root.data).append(" "); // 기존 데이터 출력
+            // 덱에 아무것도 없다면 그냥 집어넣기
+            if (deque.isEmpty()) {
+                deque.offer(node);
+                sb.append(node);
+            } else { // 덱에 노드가 있다면 비교 시작
+                while (!deque.isEmpty()) { // 덱이 비워지기 전까지 현재 노드와 들어있는 노드들을 비교한다. 만약 현재 노드가 더 크다면 그 자리에서 바로 브레이크.
+//                    System.out.println(deque);
+                    // 가장 최근에 들어간 노드 꺼내기
+                    Node last = deque.getLast();
+                    // 현재 노드랑 비교 후 현재노드가 더 작다면 최근 노드 삭제
+                    if (input < last.data) {
+                        deque.pollLast();
+                    } else {
+                        // 큐에 지정된 슬라이딩 윈도우 크기 범위에 벗어난 현재 노드라면 가장 오래된 노드를
+                        // 제거
+                        if (node.index - deque.getFirst().index >= L) {
+                            deque.pollFirst();
+                        }
+                        // 현재 노드가 더 크다면 아무런 반응 없이 그냥 현재노드만 덱에 삽입 후 반복문 탈출
+                        deque.offer(node);
+                        sb.append(deque.getFirst());
                         break;
                     }
-                    queue.offer(new Node(i, input));
+
+                    // 현재노드와 비교 후 덱에 아무것도 없어지게된다면 현재 노드만 삽입 후 반복문 탈출
+                    if (deque.isEmpty()) {
+                        deque.offer(node);
+                        sb.append(node);
+                        break;
+                    }
                 }
             }
         }
