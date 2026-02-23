01c699f47b34f495eee360945c88a554b76927a4
roa7717 <roa7717@kcert.co.kr>
2026-02-23 17:21:19 +0900

    버블 소트 학습

---
+++ src/bubble_sort/BaekJoon_2750.java
@@ -0,0 +1,33 @@
+package bubble_sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon_2750 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : nums) {
+            sb.append(num).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
