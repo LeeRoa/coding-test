d440d660d69c68cf547f0e13c1cdd4e0f1a17a18
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2026-02-25 23:45:01 +0900

    백준 9663 보류

---
+++ src/graph_search/backtracking/BaekJoon_9663.java
@@ -0,0 +1,16 @@
+package graph_search.backtracking;
+
+import java.util.*;
+import java.io.*;
+
+public class BaekJoon_9663 {
+    static int[][] board;
+    static boolean[][] V;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        V = new boolean[N][N];
+
+    }
+}
